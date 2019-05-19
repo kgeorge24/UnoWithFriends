1a2ee0c1f97fb96b29d92153d42bfaf5f4d660b7
Kyle George <k.george21@outlook.com>
2019-05-19 02:09:17 -0400

    started styling hamburger menu

--- uno-with-friends/src/components/Nav.js
+++ uno-with-friends/src/components/Nav.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { elastic as Menu } from 'react-burger-menu'
+import { slide as Menu } from 'react-burger-menu'
 import '../css/Nav.css'
 
 export default class Nav extends Component {
@@ -8,9 +8,14 @@ export default class Nav extends Component {
       <div className="nav-bar">
         <h1>Uno With Friends</h1>
         <Menu>
-          <a id="home" className="menu-item" href="/">
-            Home
-          </a>
+          <div className="username-container">
+            <h3>Username</h3>
+            <span id="username">VitoGrand24</span>{' '}
+            <span id="online-status">ONLINE</span>
+          </div>
+          <div className="friends-list-container">
+            <h3>Username</h3>
+          </div>
           <a id="signin" className="menu-item" href="/signin">
             Sign In
           </a>
